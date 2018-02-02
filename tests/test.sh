cc60520cfa65154cd3b803679a9c603d6195e9cc
Ian Leifer <ianleifer93@gmail.com>
2018-02-02 16:30:30 -0500

    Fixed test removed by chance

---
+++ tests/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ..
+./run.pl -outputTo ./tests/out1.txt -inputFrom ./tests/test1.txt
+./run.pl -outputTo ./tests/out2.txt -inputFrom ./tests/test2.txt -weighted
+./run.pl -outputTo ./tests/out3.txt -inputFrom ./tests/test3.txt -directed
+./run.pl -outputTo ./tests/out4.txt -inputFrom ./tests/test4.txt -weighted -directed
+./run.pl -outputTo ./tests/out5.txt -inputFrom ./tests/test5.txt -weighted
+cd ./tests
