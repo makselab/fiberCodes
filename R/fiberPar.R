0d2dbf2e4064558ff2bfee85ffead71b5a0069d0
Ian Leifer <ianleifer93@gmail.com>
2019-03-01 15:46:27 -0500

    Small comments added

--- R/fiberPar.R
+++ R/fiberPar.R
@@ -2,6 +2,7 @@ library(tidyr)
 library(dplyr)
 source("~/Dropbox/groupoid_finding_codes/fibers/R/functionsPar.R")
 
+# TODO: this all code can be made much faster with a proper use of igraph. I would rewrite the whole code
 runFiberCode <- function(fileNames, configuration, parId) {
   currentDirectory <- getwd()
   setwd("~/Dropbox/groupoid_finding_codes/fibers/R")
@@ -18,17 +19,19 @@ runFiberCode <- function(fileNames, configuration, parId) {
   writeToAdjacencyFile(configuration, nodeMap, weightMap, connectivity, fileNames)
 
   codePreactions(fileNames)
-  # TODO: make the key to recompile the code or not
   # TODO: properly check if code returned 1
   system(paste("g++ -std=c++11 main.cpp processor.cpp node.cpp blocks.cpp -o exec", parId, sep = ""))
+  print(paste("Process ", parId, ": Running code", sep = ""))
   system(paste("./exec", parId, " ", parId, sep = ""))
   system(paste("rm exec", parId, sep = ""))
 
+  print(paste("Process ", parId, ": Reading code output", sep = ""))
   nodeMap <- getFibersFromCodeOutput(nodeMap, fileNames)
   fibers <- prepareFibersOutput(nodeMap)
   if(configuration$BuildingBlocks == "1") {
     buildingBlocks <- getBuildingBlocksFromCodeOutput(nodeMap, fileNames)
   }
+  print(paste("Process ", parId, ": Writing to output files", sep = ""))
   writeOutputToFiles(configuration, fibers, buildingBlocks, nodeMap, network, fileNames)
   setwd(currentDirectory)
 }
