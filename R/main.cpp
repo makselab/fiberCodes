eeef254b4efcd9864cfb9604fc3d77bb7629dcf2
Ian Leifer <ianleifer93@gmail.com>
2019-04-17 16:06:48 -0400

    Fixed running of one network

--- R/main.cpp
+++ R/main.cpp
@@ -4,7 +4,12 @@
 #include <cstdlib>
 
 int main(int argc, char *argv[]) {
-	Processor *processor = processor->getProcessor(atoi(argv[1]));
+  Processor *processor;
+  int processId = -1;
+  if(argc == 2) {
+    processId = atoi(argv[1]);
+  }
+  processor = processor->getProcessor(processId);
 	processor->run();
 	return 0;
 }
