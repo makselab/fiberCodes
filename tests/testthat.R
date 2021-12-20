655dda53271154b8dafdea73f1b93ee96c918570
Ian Leifer <ianleifer93@gmail.com>
2021-12-20 14:57:34 -0500

    Removed all files. Git repo is moving

--- tests/testthat.R
+++
@@ -1,8 +0,0 @@
-library(testthat)
-
-file.remove("~/Dropbox/groupoid_finding_codes/fibers/tests/testthat/adjacency.txt")
-file.remove("~/Dropbox/groupoid_finding_codes/fibers/tests/testthat/buildingBlocks.txt")
-file.remove("~/Dropbox/groupoid_finding_codes/fibers/tests/testthat/fibers.txt")
-file.remove("~/Dropbox/groupoid_finding_codes/fibers/tests/testthat/exec")
-system("g++ -std=c++11 ~/Dropbox/groupoid_finding_codes/fibers/R/main.cpp ~/Dropbox/groupoid_finding_codes/fibers/R/processor.cpp ~/Dropbox/groupoid_finding_codes/fibers/R/node.cpp ~/Dropbox/groupoid_finding_codes/fibers/R/blocks.cpp -o ~/Dropbox/groupoid_finding_codes/fibers/tests/testthat/exec")
-test_dir("~/Dropbox/groupoid_finding_codes/fibers/tests/testthat")
